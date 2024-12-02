2a00d6875633ea8491d81ac03835af82afc6f270
Owen <owen0879@gmail.com>
2024-12-02 23:32:37 +0000

    underline

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,10 @@ import Moneybag from "./components/moneyBag";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div>
       <h1 className='text-4xl font-medium'>Money Bags</h1>
+      <div className='border-t-4 border-[#4BC789] w-[210px]'></div>
+      </div>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Moneybag name={'hello bee!'}/>
       </main>
