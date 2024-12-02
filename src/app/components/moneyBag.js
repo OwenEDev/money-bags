9dfd20fe773424c456fad06809dabe89fa5819cd
Owen <owen0879@gmail.com>
2024-12-02 23:20:52 +0000

    init commit

---
+++ src/app/components/moneyBag.js
@@ -0,0 +1,8 @@
+const Moneybag = ({name}) => {
+    return (
+        <div className="flex items-center justify-center p-3 h-[250px] w-[200px] border-radius-10  rounded-md bg-[#4BC789]">
+            <h1 className="font-bold text-slate-50 text-2xl">{name}</h1>
+        </div>
+    )
+}
+export default Moneybag;
