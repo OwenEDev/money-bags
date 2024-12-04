8afaf11c27d2d8c66b519d8a84c743d8a0eb37d3
Owen <owen0879@gmail.com>
2024-12-04 23:37:18 +0000

    added input and some state management (indexing is broken)

--- src/app/components/moneyBag.js
+++ src/app/components/moneyBag.js
@@ -1,14 +1,28 @@
-const Moneybag = ({name}) => {
+import {useState, useEffect} from "react";
+
+const Moneybag = ({name, index, edit, del} ) => {
+    const [text, setText] = useState('');
+
+    const handleTextChange = (e) => {
+        setText(e.target.value);
+    };
+
+    useEffect(() => {
+        edit(index, text, 0)
+    }, [text])
+
     return (
-        <div className="flex-col content-center h-[250px] w-[200px] border-radius-10  rounded-md bg-[#4BC789]">
+        <div className="flex-col content-center h-[250px] w-[200px] rounded-3xl bg-[#4BC789]">
             <div className="my-[10px]">
-            <h1 className="text-center font-bold text-slate-50 text-6xl">£47</h1>
-            <h1 className="text-center font-bold text-slate-50 text-2xl">{name}</h1>
+            <h1 className="text-center font-bold text-slate-50 text-6xl">{index}</h1>
+            {/* <h1 className="text-center font-bold text-slate-50 text-2xl">{name}</h1> */}
+            <input type="text" value={text} placeholder="Enter Name" onChange={(e) => {handleTextChange(e)}}/>
             </div>
             
             <div className="my-[10px] mx-10">
             <button className="border-2 hover:bg-[#82D9AD] text-white font-medium px-1 rounded w-[100%] my-[5px]">Add Purchase</button>
             <button className="border-2 hover:bg-[#82D9AD] text-white font-medium px-1 rounded w-[100%] my-[1px]">Summary</button>
+            <button className="border-2 hover:bg-[#82D9AD] text-white font-medium px-1 rounded w-[100%] my-[1px]" onClick={() => {del(index)}}>Del</button>
             </div>
             
         </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,34 @@
+"use client"
 import Image from "next/image";
 import Moneybag from "./components/moneyBag";
+import {useState} from "react";
+import { Fragment } from "react";
 
 export default function Home() {
+  const [bags, setBags] = useState([])
+  const addMoneyBag = () => {
+    let index = bags.length;
+    setBags([...bags, {name: "bonk", amount: "", index: index}])
+  }
+  const editMoneyBag = (index, name, amount) => {
+    let bagCopy = [...bags];
+    bagCopy.map((bag) => {
+      if (bag.index === index) {
+        console.log(`${bag.index} matched with ${index}`)
+      }
+    });
+  }
+  const deleteMoneyBag = (index) => {
+    console.log(index);
+    let bagCopy = [...bags];
+    let newBagArr = [];
+    bagCopy.map((bag) => {
+      if (bag.index === index) {
+        bagCopy = bagCopy.filter((item) => item.index !== index)
+      } 
+    });
+    setBags([...bagCopy]);
+  }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div>
@@ -9,10 +36,13 @@ export default function Home() {
       <div className='border-t-4 border-[#4BC789] w-[210px]'></div>
       </div>
       <main className="flex flex-row gap-8 row-start-2 items-center sm:items-start">
-        <Moneybag name={'Groceries'}/>
-        <Moneybag name={'Entertainment'}></Moneybag>
+        {bags.map((item, index) => (
+          <Fragment key={index}>
+          <Moneybag name={item.name} index={item.index} edit={editMoneyBag} del={deleteMoneyBag}/>
+          </Fragment>
+        ))}
         <div className="flex items-center justify-center content-center h-[250px] w-[200px]">
-          <button className="border-2 hover:bg-[#82D9AD] font-medium px-1 rounded">Add Money bag</button>
+          <button onClick={() => {addMoneyBag()}} className="border-2 hover:bg-[#82D9AD] font-medium px-1 rounded">Add Money bag</button>
         </div>
       </main>
     </div>
