80083238fd68a0504d79c8fe39cc5bc400e4e69b
Owen <owen0879@gmail.com>
2024-12-05 16:34:29 +0000

    popup refining

--- src/app/components/addMoneyBag.js
+++ src/app/components/addMoneyBag.js
@@ -1,15 +1,15 @@
-const AddMoneyBag = ({visibility}) => {
+const AddMoneyBag = ({visibility, hide}) => {
     return (
-        <div tabIndex="-1" aria-hidden="true" className={`${visibility} overflow-y-auto overflow-x-hidden fixed justify-center items-center w-[50%] md:inset-0 h-[calc(100%-1rem)] max-h-full my-auto mx-[25%]`}>
-    <div className="relative p-4 w-full max-w-2xl max-h-full">
+        <div tabIndex="-1" aria-hidden="true" className={`${visibility} overflow-y-auto overflow-x-hidden fixed justify-center items-center w-[50%] md:inset-0 h-[calc(100%-1rem)] max-h-full my-[100px] mx-[25%]`}>
+    <div className="relative p-4 w-full max-h-full">
         <div className="relative bg-white rounded-lg">
             <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
                 <h3 className="text-xl font-semibold text-black">
                     Add Moneybag
                 </h3>
-                <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+                <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-[#4BC789]" onClick={() => {hide()}}>
                     <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                     </svg>
                     <span className="sr-only">Close modal</span>
                 </button>
--- src/app/page.js
+++ src/app/page.js
@@ -30,6 +30,9 @@ export default function Home() {
     });
     setBags([...bagCopy]);
   }
+  const hidePopup = () => {
+    setPopupVisibility('hidden');
+  }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div>
@@ -37,7 +40,7 @@ export default function Home() {
       <div className='border-t-4 border-[#4BC789] w-[210px]'></div>
       </div>
       <main className="flex flex-row gap-8 row-start-2 items-center sm:items-start">
-        <AddMoneyBag visibility={popupVisibility}/>
+        <AddMoneyBag visibility={popupVisibility} hide={hidePopup}/>
         {bags.map((item, index) => (
           <Fragment key={index}>
           <Moneybag name={item.name} index={item.index} edit={editMoneyBag} del={deleteMoneyBag}/>
